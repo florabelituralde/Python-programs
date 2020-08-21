dd88f759281d88ec543a807a5fcac7ace5069e6d
Florabel Ituralde <67927764+florabelituralde@users.noreply.github.com>
2020-08-20 21:49:55 -0400

    upload files

---
+++ Classes.py
@@ -0,0 +1,9 @@
+class Point():
+    def_init_(self, x, y):
+        self.x = x
+        self.y = y
+
+p = Point(2, 8)
+print(p.x)
+print(p.y)
+
---
+++ Hello.py
@@ -0,0 +1,4 @@
+print("Hello, World!")
+
+name = input("Name: ")
+print("Hello, "+name)
